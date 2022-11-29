74ed167bee7e57a603fec9fbe9eabc50806288d0
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-29 17:00:07 +0100

    componenti header e main

---
+++ vue-boolfix/src/components/HeaderComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="bg-dark d-flex justify-content-between align-items-center p-3">
+        <div>
+          <h2 class="text-danger fw-semibold">BOOLFLIX</h2>
+        </div>
+        <div class="d-flex">
+            <input type="text">
+            <button class="ms-3">Cerca</button>
+        </div>
+        
+    </header>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComp',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+button{
+    border: transparent;
+}
+
+button:hover{
+    background-color: rgba(255, 0, 0, 0.7);
+}
+
+</style>
